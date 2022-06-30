a4dc5d8ca27b2f43ca5716197cc70ae8890cbd20
Joseph Ingio <josephingio@gmail.com>
2022-06-30 11:14:27 -0700

    4th task

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+
+
+/**
+ *
+ *  * main - Entry point
+ *
+ *
+ *
+ *    * Return: Always 0 (Success)
+ *
+*/
+
+int main(void)
+
+{
+
+		puts("\"Programming is like building a multilingu			al puzzle");
+
+			return (0);
+
+}
