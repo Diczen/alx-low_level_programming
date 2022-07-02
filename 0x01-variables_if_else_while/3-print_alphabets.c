68367ae4c8d31ae564ba6f8f03d6cb6c8997b776
Joseph Ingio <josephingio@gmail.com>
2022-07-02 06:23:18 -0700

    3rd commit

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -11,11 +11,18 @@
  */int main(void)
 {
 	char c = 'a';
+	char d = 'A'
 
 	while (c <= 'z')
 	{
 		putchar(c);
 		c++;
+	}
+
+	while (d <= 'Z')
+	{
+		putchar(d)
+		d++
 	}
 	putchar('\n');
 	return (0);
