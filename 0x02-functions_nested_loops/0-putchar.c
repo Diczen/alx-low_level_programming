ea09f14d26113e456643b3c06e246f56a8c46461
Joseph Ingio <josephingio@gmail.com>
2022-07-05 16:27:16 -0700

    task zero

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/*
+*this is function that prints "_putchar"
+*
+* returns nothing
+*
+*/void niceprogram(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/main.c
@@ -0,0 +1,13 @@
+#include"main.h"
+/*
+ *
+ * this is the main funtionc
+ *
+ * it returns 0
+ *
+ */int main(void)
+{
+	niceprogram();
+
+	return (0);
+}
