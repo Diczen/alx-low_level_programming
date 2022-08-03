558389dfcb82b3672e00a4e7a1774415423bc9ec
Joseph Ingio <josephingio@gmail.com>
2022-08-03 09:58:49 -0700

    one

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,15 +1,16 @@
 #include "function_pointers.h"
+
 /**
  * print_name - prints a name
  * @name:name to print
  * @f: function which print name
- * Return: no return
  */
 
 void print_name(char *name, void (*f)(char *))
 {
-	if (name == NULL || f == NULL)
-		return;
 
-	f(name);
+if (name == NULL || f == NULL)
+return;
+
+f(name);
 }
