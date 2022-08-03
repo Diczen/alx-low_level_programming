15e9482ee5729dfd55ee33c492ea45bf290a3708
Joseph Ingio <josephingio@gmail.com>
2022-08-03 09:45:27 -0700

    cool

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,11 +1,9 @@
 #include "function_pointers.h"
-
 /**
- *
  * print_name - prints a name
- *
  * @name:name to print
- * @f: function which print name*
+ * @f: function which print name
+ * Return: no return
  */
 
 void print_name(char *name, void (*f)(char *))
